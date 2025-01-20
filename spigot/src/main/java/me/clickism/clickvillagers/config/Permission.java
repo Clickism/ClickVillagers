934aafab02f07cc4822561c38643441a29bf08c6
Clickism <yagizaktasx@gmail.com>
2025-01-20 22:35:23 +0100

    add reload permission and use command sender

--- spigot/src/main/java/me/clickism/clickvillagers/config/Permission.java
+++ spigot/src/main/java/me/clickism/clickvillagers/config/Permission.java
@@ -7,6 +7,7 @@
 package me.clickism.clickvillagers.config;
 
 import me.clickism.clickvillagers.message.Message;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 
 public enum Permission {
@@ -17,7 +18,8 @@ public enum Permission {
     CLAIM,
     PARTNER,
     BYPASS_CLAIMS,
-    BYPASS_LIMITS;
+    BYPASS_LIMITS,
+    RELOAD;
 
     private static final String PLUGIN_PREFIX = "clickvillagers";
     private final String permission;
@@ -27,17 +29,17 @@ public enum Permission {
         permission = PLUGIN_PREFIX + "." + name;
     }
 
-    public boolean has(Player player) {
-        return player.hasPermission(permission);
+    public boolean has(CommandSender sender) {
+        return sender.hasPermission(permission);
     }
 
-    public boolean lacks(Player player) {
-        return !has(player);
+    public boolean lacks(CommandSender sender) {
+        return !has(sender);
     }
 
-    public boolean lacksAndNotify(Player player) {
-        if (lacks(player)) {
-            Message.NO_PERMISSION.send(player);
+    public boolean lacksAndNotify(CommandSender sender) {
+        if (lacks(sender)) {
+            Message.NO_PERMISSION.send(sender);
             return true;
         }
         return false;
