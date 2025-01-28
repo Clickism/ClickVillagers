d3d3056f21d02689f7a6ff1087abe31192a5b9e1
Clickism <yagizaktasx@gmail.com>
2025-01-29 00:51:33 +0100

    remove old license header

--- fabric/src/main/java/me/clickism/clickvillagers/util/CodecUtils.java
+++ fabric/src/main/java/me/clickism/clickvillagers/util/CodecUtils.java
@@ -1,9 +1,3 @@
-/*
- * Copyright 2020-2025 Clickism
- * Released under the GNU General Public License 3.0.
- * See LICENSE.md for details.
- */
-
 package me.clickism.clickvillagers.util;
 
 import com.mojang.datafixers.util.Either;
