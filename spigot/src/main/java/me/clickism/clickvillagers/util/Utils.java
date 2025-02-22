4a1f1f367e64661c72375b31532271537224f8c0
Clickism <yagizaktasx@gmail.com>
2025-02-22 23:31:34 +0100

    add capitalization utils

--- spigot/src/main/java/me/clickism/clickvillagers/util/Utils.java
+++ spigot/src/main/java/me/clickism/clickvillagers/util/Utils.java
@@ -27,6 +27,36 @@ public class Utils {
         return ChatColor.translateAlternateColorCodes('&', text);
     }
 
+    public static String formatMaterial(Material material) {
+        return titleCase(material.toString().replace("_", " "));
+    }
+
+    /**
+     * Title cases a string. (i.E.: "HELLO world" -> "Hello World")
+     *
+     * @param string the string to title case
+     * @return the title cased string
+     */
+    public static String titleCase(String string) {
+        return capitalize(string.toLowerCase());
+    }
+
+    /**
+     * Capitalizes the first letter of each word in a string.
+     *
+     * @param string the string to capitalize
+     * @return the capitalized string
+     */
+    public static String capitalize(String string) {
+        String[] words = string.split(" ");
+        StringBuilder capitalizedString = new StringBuilder();
+        for (String word : words) {
+            if (word.isEmpty()) continue;
+            capitalizedString.append(capitalizeWord(word)).append(" ");
+        }
+        return capitalizedString.toString().trim();
+    }
+
     public static String capitalizeWord(String string) {
         return string.substring(0, 1).toUpperCase() + string.substring(1);
     }
