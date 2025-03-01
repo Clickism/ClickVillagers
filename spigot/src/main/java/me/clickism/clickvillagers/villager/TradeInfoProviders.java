3c8e4c6edb29c1eefa7e66dc0fde5bd5b319c951
Clickism <yagizaktasx@gmail.com>
2025-03-01 11:28:13 +0300

    add more professions

--- spigot/src/main/java/me/clickism/clickvillagers/villager/TradeInfoProviders.java
+++ spigot/src/main/java/me/clickism/clickvillagers/villager/TradeInfoProviders.java
@@ -9,13 +9,25 @@ package me.clickism.clickvillagers.villager;
 import me.clickism.clickvillagers.util.Utils;
 import org.bukkit.Material;
 import org.bukkit.entity.Villager;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.EnchantmentStorageMeta;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Set;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class TradeInfoProviders {
+
+    private static final Function<ItemStack, String> ITEM_FORMATTER = item -> {
+        String prefix = getPrefix(item.getType());
+        if (prefix == null) {
+            return TradeInfoProvider.DEFAULT_FORMATTER.apply(item);
+        }
+        return prefix + " " + Utils.formatItem(item);
+    };
 
     public static final TradeInfoProvider ALL_TRADES = TradeInfoProvider.builder()
             .filterIngredients(item -> true)
@@ -43,42 +55,28 @@ public class TradeInfoProviders {
     public static final TradeInfoProvider FARMER = TradeInfoProvider.builder()
             .acceptIngredients(Material.WHEAT, Material.BEETROOT, Material.CARROT, Material.POTATO, Material.PUMPKIN, Material.MELON)
             .acceptResults(Material.GOLDEN_CARROT, Material.APPLE, Material.BREAD)
-            .resultFormatter(item -> {
-                int amount = item.getAmount();
-                return switch (item.getType()) {
-                    case GOLDEN_CARROT -> "&e🥕 " + amount + " Golden Carrot";
-                    case APPLE -> "&c🍎 " + amount + " Apple";
-                    default -> TradeInfoProvider.DEFAULT_FORMATTER.apply(item);
-                };
-            })
-            .ingredientFormatter(item -> {
-                int amount = item.getAmount();
-                return switch (item.getType()) {
-                    case WHEAT -> "&e🌾 " + amount + " Wheat";
-                    case CARROT -> "&6🥕 " + amount + " Carrot";
-                    case POTATO -> "&6🥔 " + amount + " Potato";
-                    case PUMPKIN -> "&6🎃 " + amount + " Pumpkin";
-                    case MELON -> "&a🍉 " + amount + " Melon";
-                    default -> TradeInfoProvider.DEFAULT_FORMATTER.apply(item);
-                };
-            })
+            .itemFormatter(ITEM_FORMATTER)
             .build();
 
     private static final Set<Material> DIAMOND_TOOLS = Set.of(Material.DIAMOND_AXE, Material.DIAMOND_PICKAXE,
             Material.DIAMOND_SHOVEL, Material.DIAMOND_HOE, Material.DIAMOND_SWORD);
 
+    private static final Set<Material> DIAMOND_ARMOR = Set.of(Material.DIAMOND_HELMET,
+            Material.DIAMOND_CHESTPLATE, Material.DIAMOND_LEGGINGS, Material.DIAMOND_BOOTS);
+
+    private static final Set<Material> DIAMOND_ITEMS = Stream.concat(DIAMOND_TOOLS.stream(), DIAMOND_ARMOR.stream())
+            .collect(Collectors.toSet());
+
     public static final TradeInfoProvider SMITH = TradeInfoProvider.builder()
             .acceptIngredients(Material.COAL)
-            .acceptResults(DIAMOND_TOOLS.toArray(new Material[0]))
-            .ingredientFormatter(item -> {
-                if (item.getType() != Material.COAL) return TradeInfoProvider.DEFAULT_FORMATTER.apply(item);
-                return "&8🪨 " + item.getAmount() + " Coal";
-            })
+            .acceptResults(Stream.concat(DIAMOND_ITEMS.stream(), Stream.of(Material.SHIELD))
+                    .toArray(Material[]::new))
+            .ingredientFormatter(ITEM_FORMATTER)
             .resultFormatter(item -> {
                 Material material = item.getType();
-                if (!DIAMOND_TOOLS.contains(material)) return TradeInfoProvider.DEFAULT_FORMATTER.apply(item);
+                if (!DIAMOND_ITEMS.contains(material)) return ITEM_FORMATTER.apply(item);
                 ItemMeta meta = item.getItemMeta();
-                if (meta == null) return TradeInfoProvider.DEFAULT_FORMATTER.apply(item);
+                if (meta == null) return ITEM_FORMATTER.apply(item);
                 String enchantments = meta.getEnchants().entrySet().stream()
                         .map(entry -> {
                             String enchantment = entry.getKey().getKey().getKey().replace("_", " ");
@@ -86,18 +84,71 @@ public class TradeInfoProviders {
                             return "\n                        &d   &7" + Utils.titleCase(enchantment) + " " + level;
                         })
                         .collect(Collectors.joining());
-                String icon = (material == Material.DIAMOND_SWORD) ? "🗡" : "⛏";
+                String icon = "";
+                if (DIAMOND_TOOLS.contains(material)) {
+                    icon = (material == Material.DIAMOND_SWORD) ? "🗡" : "⛏";
+                } else {
+                    icon = "👕";
+                }
                 String name = "&b" + icon + " " + Utils.formatMaterial(material);
                 return name + enchantments;
             })
+            .build();
+
+    public static final TradeInfoProvider BUTCHER = TradeInfoProvider.builder()
+            .acceptIngredients(Material.CHICKEN, Material.PORKCHOP, Material.RABBIT,
+                    Material.SWEET_BERRIES, Material.KELP)
+            .acceptResults(Material.COOKED_CHICKEN, Material.COOKED_PORKCHOP)
+            .itemFormatter(ITEM_FORMATTER)
+            .build();
+
+    public static final TradeInfoProvider FISHERMAN = TradeInfoProvider.builder()
+            .acceptIngredients(Material.STRING, Material.COAL)
+            .acceptResults(Material.CAMPFIRE)
+            .itemFormatter(ITEM_FORMATTER)
             .build();
 
     public static TradeInfoProvider getProvider(Villager.Profession profession) {
         return switch (profession) {
             case LIBRARIAN -> LIBRARIAN;
             case FARMER -> FARMER;
-            case TOOLSMITH, WEAPONSMITH -> SMITH;
+            case TOOLSMITH,
+                 WEAPONSMITH,
+                 ARMORER -> SMITH;
+            case BUTCHER -> BUTCHER;
             default -> ALL_TRADES;
+        };
+    }
+
+    @Nullable
+    private static String getPrefix(Material material) {
+        return switch (material) {
+            case GOLDEN_CARROT -> "&e🥕";
+            case APPLE -> "&c🍎";
+
+            case WHEAT -> "&e🌾";
+            case CARROT -> "&6🥕";
+            case POTATO -> "&6🥔";
+            case PUMPKIN -> "&6🎃";
+            case MELON -> "&a🍉";
+
+            case COAL -> "&8🪨";
+
+            case CHICKEN -> "&e🐓";
+            case PORKCHOP -> "&e🐷";
+            case RABBIT -> "&e🐰";
+            case SWEET_BERRIES -> "&c🍇";
+            case KELP -> "&a🌿";
+
+            case COOKED_CHICKEN -> "&6🐓";
+            case COOKED_PORKCHOP -> "&6🐷";
+
+            case STRING -> "&f🪢";
+            case CAMPFIRE -> "&🔥";
+
+            case SHIELD -> "&6🛡️";
+
+            default -> null;
         };
     }
 
