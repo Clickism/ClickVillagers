1ce6ef5524cb441d3c0d5f9575445c21d32eed4b
Clickism <yagizaktasx@gmail.com>
2025-03-08 21:15:10 +0300

    move roman numeral converter

--- spigot/src/main/java/me/clickism/clickvillagers/util/Utils.java
+++ spigot/src/main/java/me/clickism/clickvillagers/util/Utils.java
@@ -97,4 +97,15 @@ public class Utils {
             player.getWorld().dropItem(player.getLocation(), toDrop);
         });
     }
+
+    public static String toRomanNumeral(int number) {
+        return switch (number) {
+            case 1 -> "I";
+            case 2 -> "II";
+            case 3 -> "III";
+            case 4 -> "IV";
+            case 5 -> "V";
+            default -> String.valueOf(number);
+        };
+    }
 }
