2be29fe0522c312b3b28e9688d144925da927f68
Clickism <yagizaktasx@gmail.com>
2025-04-03 15:30:37 +0200

    use helper methods to convert to ClaimedVillagerData

--- fabric/src/main/java/me/clickism/clickvillagers/villager/VillagerHandler.java
+++ fabric/src/main/java/me/clickism/clickvillagers/villager/VillagerHandler.java
@@ -22,12 +22,14 @@ public class VillagerHandler<T extends LivingEntity & VillagerDataContainer> {
     }
 
     public void setOwner(UUID uuid) {
-        ((ClaimedVillagerData) entity.getVillagerData()).clickVillagers_Fabric$setOwner(uuid);
+        ClaimedVillagerData.of(entity.getVillagerData())
+                .clickVillagers_Fabric$setOwner(uuid);
     }
     
     @Nullable
     public UUID getOwner() {
-        return ((ClaimedVillagerData) entity.getVillagerData()).clickVillagers_Fabric$getOwner();
+        return ClaimedVillagerData.of(entity.getVillagerData())
+                .clickVillagers_Fabric$getOwner();
     }
     
     public boolean hasOwner() {
@@ -39,10 +41,12 @@ public class VillagerHandler<T extends LivingEntity & VillagerDataContainer> {
     }
     
     public boolean isTradingOpen() {
-        return ((ClaimedVillagerData) entity.getVillagerData()).clickVillagers_Fabric$isTradingOpen();
+        return ClaimedVillagerData.of(entity.getVillagerData())
+                .clickVillagers_Fabric$isTradingOpen();
     }
     
     public void setTradingOpen(boolean open) {
-        ((ClaimedVillagerData) entity.getVillagerData()).clickVillagers_Fabric$setTradingOpen(open);
+        ClaimedVillagerData.of(entity.getVillagerData())
+                .clickVillagers_Fabric$setTradingOpen(open);
     }
 }
