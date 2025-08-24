9068633f66f196ee5e7ec28cb4769f8303223ebc
Clickism <yagizaktasx@gmail.com>
2025-08-24 11:03:02 +0200

    update stonecutter

--- fabric/stonecutter.gradle.kts
+++ fabric/stonecutter.gradle.kts
@@ -2,13 +2,3 @@ plugins {
     id("dev.kikugie.stonecutter")
 }
 stonecutter active "1.21.8" /* [SC] DO NOT EDIT */
-
-stonecutter registerChiseled tasks.register("chiseledBuild", stonecutter.chiseled) { 
-    group = "project"
-    ofTask("build")
-}
-
-stonecutter registerChiseled tasks.register("publishAllVersions", stonecutter.chiseled) {
-    group = "project"
-    ofTask("publishMods")
-}
