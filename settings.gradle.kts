4a06f52c1df81a3b8e7197d90dc0e16d376ea307
Clickism <yagizaktasx@gmail.com>
2025-10-02 13:09:34 +0200

    add stonecutter version for 1.21.9

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,16 +1,16 @@
 pluginManagement {
-	repositories {
-		maven {
-			name = "Fabric"
-			url = uri("https://maven.fabricmc.net/")
-		}
-		mavenCentral()
-		gradlePluginPortal()
-	}
+    repositories {
+        maven {
+            name = "Fabric"
+            url = uri("https://maven.fabricmc.net/")
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
 }
 
 plugins {
-	id("dev.kikugie.stonecutter") version "0.7.8"
+    id("dev.kikugie.stonecutter") version "0.7.8"
 }
 
 rootProject.name = "ClickVillagers"
@@ -18,10 +18,10 @@ rootProject.name = "ClickVillagers"
 include("fabric", "spigot")
 
 stonecutter {
-	kotlinController = true
-	centralScript = "build.gradle.kts"
-	create("fabric") {
-		versions("1.21.8", "1.21.5", "1.21.4", "1.21.1", "1.20.1")
-		vcsVersion = "1.21.8"
-	}
+    kotlinController = true
+    centralScript = "build.gradle.kts"
+    create("fabric") {
+        versions("1.21.9", "1.21.8", "1.21.5", "1.21.4", "1.21.1", "1.20.1")
+        vcsVersion = "1.21.8"
+    }
 }
