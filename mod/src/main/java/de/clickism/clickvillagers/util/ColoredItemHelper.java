a3260a994787eb09ed5eabd249a78c2550943399
Clickism <yagizaktasx@gmail.com>
2026-06-23 13:50:44 +0200

    fix reflection bug

--- mod/src/main/java/de/clickism/clickvillagers/util/ColoredItemHelper.java
+++ mod/src/main/java/de/clickism/clickvillagers/util/ColoredItemHelper.java
@@ -20,10 +20,82 @@ public class ColoredItemHelper {
     public static final ColorCollection<Item> DYED_TERRACOTTA = Items.DYED_TERRACOTTA;
     public static final ColorCollection<Item> WOOL = Items.WOOL;
     //?} else {
-    /*public static final ColorCollection<Item> STAINED_GLASS_PANE = ColorCollection.fromFieldsOfClass(Items.class, "STAINED_GLASS_PANE");
-    public static final ColorCollection<Item> GLAZED_TERRACOTTA = ColorCollection.fromFieldsOfClass(Items.class, "GLAZED_TERRACOTTA");
-    public static final ColorCollection<Item> DYED_TERRACOTTA = ColorCollection.fromFieldsOfClass(Items.class, "TERRACOTTA");
-    public static final ColorCollection<Item> WOOL = ColorCollection.fromFieldsOfClass(Items.class, "WOOL");
+
+    /*public static final ColorCollection<Item> STAINED_GLASS_PANE = new ColorCollection<>(
+            Items.WHITE_STAINED_GLASS_PANE,
+            Items.ORANGE_STAINED_GLASS_PANE,
+            Items.MAGENTA_STAINED_GLASS_PANE,
+            Items.LIGHT_BLUE_STAINED_GLASS_PANE,
+            Items.YELLOW_STAINED_GLASS_PANE,
+            Items.LIME_STAINED_GLASS_PANE,
+            Items.PINK_STAINED_GLASS_PANE,
+            Items.GRAY_STAINED_GLASS_PANE,
+            Items.LIGHT_GRAY_STAINED_GLASS_PANE,
+            Items.CYAN_STAINED_GLASS_PANE,
+            Items.PURPLE_STAINED_GLASS_PANE,
+            Items.BLUE_STAINED_GLASS_PANE,
+            Items.BROWN_STAINED_GLASS_PANE,
+            Items.GREEN_STAINED_GLASS_PANE,
+            Items.RED_STAINED_GLASS_PANE,
+            Items.BLACK_STAINED_GLASS_PANE
+    );
+
+    public static final ColorCollection<Item> GLAZED_TERRACOTTA = new ColorCollection<>(
+            Items.WHITE_GLAZED_TERRACOTTA,
+            Items.ORANGE_GLAZED_TERRACOTTA,
+            Items.MAGENTA_GLAZED_TERRACOTTA,
+            Items.LIGHT_BLUE_GLAZED_TERRACOTTA,
+            Items.YELLOW_GLAZED_TERRACOTTA,
+            Items.LIME_GLAZED_TERRACOTTA,
+            Items.PINK_GLAZED_TERRACOTTA,
+            Items.GRAY_GLAZED_TERRACOTTA,
+            Items.LIGHT_GRAY_GLAZED_TERRACOTTA,
+            Items.CYAN_GLAZED_TERRACOTTA,
+            Items.PURPLE_GLAZED_TERRACOTTA,
+            Items.BLUE_GLAZED_TERRACOTTA,
+            Items.BROWN_GLAZED_TERRACOTTA,
+            Items.GREEN_GLAZED_TERRACOTTA,
+            Items.RED_GLAZED_TERRACOTTA,
+            Items.BLACK_GLAZED_TERRACOTTA
+    );
+
+    public static final ColorCollection<Item> DYED_TERRACOTTA = new ColorCollection<>(
+            Items.WHITE_TERRACOTTA,
+            Items.ORANGE_TERRACOTTA,
+            Items.MAGENTA_TERRACOTTA,
+            Items.LIGHT_BLUE_TERRACOTTA,
+            Items.YELLOW_TERRACOTTA,
+            Items.LIME_TERRACOTTA,
+            Items.PINK_TERRACOTTA,
+            Items.GRAY_TERRACOTTA,
+            Items.LIGHT_GRAY_TERRACOTTA,
+            Items.CYAN_TERRACOTTA,
+            Items.PURPLE_TERRACOTTA,
+            Items.BLUE_TERRACOTTA,
+            Items.BROWN_TERRACOTTA,
+            Items.GREEN_TERRACOTTA,
+            Items.RED_TERRACOTTA,
+            Items.BLACK_TERRACOTTA
+    );
+
+    public static final ColorCollection<Item> WOOL = new ColorCollection<>(
+            Items.WHITE_WOOL,
+            Items.ORANGE_WOOL,
+            Items.MAGENTA_WOOL,
+            Items.LIGHT_BLUE_WOOL,
+            Items.YELLOW_WOOL,
+            Items.LIME_WOOL,
+            Items.PINK_WOOL,
+            Items.GRAY_WOOL,
+            Items.LIGHT_GRAY_WOOL,
+            Items.CYAN_WOOL,
+            Items.PURPLE_WOOL,
+            Items.BLUE_WOOL,
+            Items.BROWN_WOOL,
+            Items.GREEN_WOOL,
+            Items.RED_WOOL,
+            Items.BLACK_WOOL
+    );
     *///?}
 
     //? if <26.2 {
@@ -47,36 +119,6 @@ public class ColoredItemHelper {
     ) {
         public List<T> asList() {
             return List.of(white, orange, magenta, lightBlue, yellow, lime, pink, gray, lightGray, cyan, purple, blue, brown, green, red, black);
-        }
-
-        public static <T> ColorCollection<T> fromFieldsOfClass(Class<?> clazz, String fieldSuffix) {
-            return new ColorCollection<>(
-                    staticField(clazz, "WHITE_" + fieldSuffix),
-                    staticField(clazz, "ORANGE_" + fieldSuffix),
-                    staticField(clazz, "MAGENTA_" + fieldSuffix),
-                    staticField(clazz, "LIGHT_BLUE_" + fieldSuffix),
-                    staticField(clazz, "YELLOW_" + fieldSuffix),
-                    staticField(clazz, "LIME_" + fieldSuffix),
-                    staticField(clazz, "PINK_" + fieldSuffix),
-                    staticField(clazz, "GRAY_" + fieldSuffix),
-                    staticField(clazz, "LIGHT_GRAY_" + fieldSuffix),
-                    staticField(clazz, "CYAN_" + fieldSuffix),
-                    staticField(clazz, "PURPLE_" + fieldSuffix),
-                    staticField(clazz, "BLUE_" + fieldSuffix),
-                    staticField(clazz, "BROWN_" + fieldSuffix),
-                    staticField(clazz, "GREEN_" + fieldSuffix),
-                    staticField(clazz, "RED_" + fieldSuffix),
-                    staticField(clazz, "BLACK_" + fieldSuffix)
-            );
-        }
-
-        @SuppressWarnings("unchecked")
-        private static <T> T staticField(Class<?> clazz, String fieldName) {
-            try {
-                return (T) clazz.getField(fieldName).get(null);
-            } catch (NoSuchFieldException | IllegalAccessException e) {
-                throw new RuntimeException(e);
-            }
         }
     }
     *///?}
